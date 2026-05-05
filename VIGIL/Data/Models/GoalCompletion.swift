ce160b5c1d09642f3566a6ca80f2c3b47d14ffbd
Ashish <abhishek@qualtechedge.com>
2026-05-06 02:56:29 +0530

    Refactor GoalCompletion model to use an initializer for property assignment, enhancing clarity and flexibility in instantiation.

--- VIGIL/Data/Models/GoalCompletion.swift
+++ VIGIL/Data/Models/GoalCompletion.swift
@@ -8,10 +8,24 @@ import SwiftData
 
 @Model
 final class GoalCompletion {
-    var id: UUID = UUID()
-    var loggedAt: Date = Date()
-    var value: Double = 0
+    var id: UUID
+    var loggedAt: Date
+    var value: Double
 
     var goal: Goal?
     var dayLog: DayLog?
+
+    init(
+        id: UUID = UUID(),
+        loggedAt: Date = Date(),
+        value: Double = 0,
+        goal: Goal? = nil,
+        dayLog: DayLog? = nil
+    ) {
+        self.id = id
+        self.loggedAt = loggedAt
+        self.value = value
+        self.goal = goal
+        self.dayLog = dayLog
+    }
 }
